fb1202d8e98751dd3af14eb3e9caf3cddd73cf15
Ivan Notaros <nothke@gmail.com>
2018-10-31 05:23:30 +0100

    Plane sounds script

---
+++ Assets/Scripts/PlaneSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneSounds : MonoBehaviour
+{
+    public Plane plane;
+    public AudioSource jet;
+    public AnimationCurve volumeCurve;
+    public AudioSource turbulence;
+    public AnimationCurve turbulenceVolumeCurve;
+
+    float dampVelo;
+    float smoothThrottle = 1;
+
+    void Update()
+    {
+        float target = plane.throttle;
+
+        float smoothing = 4;
+        if (target == 0) smoothing = 10;
+        if (plane.isPushback) smoothing = 10;
+
+        smoothThrottle = Mathf.SmoothDamp(smoothThrottle, target, ref dampVelo, smoothing);
+        jet.pitch = smoothThrottle * 1.5f;
+        jet.volume = volumeCurve.Evaluate(smoothThrottle);
+        turbulence.volume = turbulenceVolumeCurve.Evaluate(smoothThrottle);
+    }
+}
