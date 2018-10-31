101d5d2b4ed084cbbdc029bd0725717877797f4c
Ivan Notaros <nothke@gmail.com>
2018-10-31 05:23:19 +0100

    Draw gate gizmos

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -7,4 +7,12 @@ public class Gate : MonoBehaviour
 
     public Plane plane;
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, -transform.forward * 20);
+        Gizmos.DrawLine(
+            transform.position - transform.right * 4,
+            transform.position + transform.right * 4);
+    }
+
 }
